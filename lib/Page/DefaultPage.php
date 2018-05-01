367bfbff08d0ee3b8b7399c387282c5a4526207d
Jasper van Rijbroek <jasper@jvar.nl>
2018-05-01 13:37:23 +0200

    Updated iterator and related methods.

--- lib/Page/DefaultPage.php
+++ lib/Page/DefaultPage.php
@@ -13,6 +13,7 @@ use Frontender\Core\Object\MapperTrait;
 use Frontender\Core\Object\Object;
 use Frontender\Core\Parameters\ParametersTrait;
 use Frontender\Core\Template\Template;
+use Frontender\Core\Wrappers;
 
 use Slim\Container;
 use Slim\Http\Request;
@@ -75,78 +76,6 @@ class DefaultPage extends Object
         return $this;
     }
 
-    public function getModel($config, $iterator)
-    {
-        $model = null;
-
-        if (is_string($config) && $config === '@inherit') {
-            for ($i = $iterator->getDepth() - 1; $i >= 0; $i--) {
-                $parent = $iterator->getSubIterator($i);
-                $key = $parent->key();
-
-                $data = $parent->offsetGet($key);
-
-                if (is_object($data) && property_exists($data, 'data'))
-                {
-                    if (is_array($data->data) && count($data->data))
-                    {
-                        // TODO: Nested Inherit!
-                    }
-                }
-
-                if ($i === 0)
-                {
-                    if ($parent->offsetExists('data'))
-                    {
-                        $data = $parent->offsetGet('data');
-
-                        $name = $parent->offsetExists('model') ? $parent->offsetGet('model')->name : $this->getName();
-
-                        if($parent->offsetExists('model')) {
-                            $iterator->offsetSet('model', $parent->offsetGet('model'));
-                        }
-
-                        if(array_key_exists(Inflector::singularize($name), $data)) {
-                            $model = $data[Inflector::singularize($name)];
-                        }
-                    }
-                }
-            }
-        } else {
-            $params = [];
-
-            foreach($config as $key => $values) {
-                if($key === 'controls') {
-                    foreach($values as $itemKey => $value) {
-                        $params[$itemKey] = $this->getFormValue($value);
-                    }
-                }
-            }
-
-            $name = $params['name'];
-            unset($params['name']);
-
-            $state = isset($params) ? $this->mapValues($params, $iterator) : [];
-
-            // Parse the data through to the view.
-            $config->params = $state;
-
-            $state['language'] = array_key_exists('language', $state) ? $state['language'] : $this->getLocale();
-
-            $model_class = 'Prototype\\Model\\' . ucfirst($name) . 'Model';
-
-            if ($this->container->has($model_class)) {
-                $model = $this->container->get($model_class);
-                $model->setState($state, true);
-            } else {
-                $model = new $model_class($this->container);
-                $model->setState($state);
-            }
-        }
-
-        return $model;
-    }
-
     // TODO: Allow for flexiable injection!
     public function getLocale() : string
     {
@@ -159,69 +88,9 @@ class DefaultPage extends Object
             return $this->data;
         }
 
-        $iterator = new \RecursiveIteratorIterator(new \RecursiveArrayIterator($this->data), \RecursiveIteratorIterator::SELF_FIRST);
+        $this->_parseArray($this->data);
 
-        foreach ($iterator as $key => $values)
-        {
-            // Make sure to append the data key!
-            if ($key === 'template')
-            {
-                if (!$iterator->getInnerIterator()->offsetExists('data'))
-                {
-                    $iterator->getInnerIterator()->offsetSet('data', []);
-                }
-            }
-
-            if ($key === 'template_config')
-            {
-                $iterator->getSubIterator($iterator->getDepth())->offsetSet('data', (function () use ($values, $iterator) {
-                    $data = $iterator->getSubIterator($iterator->getDepth())->offsetExists('data') ?
-                        $iterator->getSubIterator($iterator->getDepth())->offsetGet('data') : [];
-
-                    foreach ($values as $key => $value)
-                    {
-                        if ($key === 'model')
-                        {
-                            $model = $this->getModel($value, $iterator);
-
-                            if ($model instanceof AbstractModel)
-                            {
-	                            $name = $model->getName();
-
-	                            if(strpos($name, 'Channel') !== false) {
-	                            	$name = strtolower(str_replace('Channel', '', $name));
-	                            }
-
-	                            if(property_exists($value->controls, 'id') && strpos($model->getName(), 'Channel') === false) {
-	                            	$name = Inflector::singularize($name);
-	                            }
-
-	                            $data[$name] = new Wrapper($model);
-                                $iterator->getSubIterator($iterator->getDepth())->offsetSet('model', $value);
-                            }
-                        } else {
-                            // Check if we have "form" elements.
-                            $data[$key] = $value;
-                        }
-                    }
-
-                    return $data;
-                })());
-
-                // Remove key when finished.
-                $iterator->getInnerIterator()->offsetUnset($key);
-            }
-
-            if($key === 'controls') {
-                $current = $iterator->getSubIterator($iterator->getDepth());
-
-                foreach($values as $valueKey => $value) {
-                    $current->offsetSet($valueKey, $this->getFormValue($value));
-                }
-            }
-        }
-
-        $parsed = true;
+        $this->_parsed = true;
     }
 
     private function getId($iterator)
@@ -286,16 +155,13 @@ class DefaultPage extends Object
     {
         $this->parseData();
 
-        $vars = array_merge($this->getParameters(), (array_merge($this->getData()->data, $data))) ?? [];
+        // Have still to figure out what this does, will most likely leave it in.
+        $vars = array_merge($this->getParameters(), (array_merge($this->getData(), $data))) ?? [];
         $this->getTemplate()->setDefaultVariables($vars);
 
-        $data = [];
+        $pageData = $this->getData();
 
-        if(array_key_exists('containers', $this->getData())) {
-            $data['containers'] = $this->getData()->containers;
-        }
-
-        return $this->template->loadFile($this->getData()->template)->render($data);
+        return $this->template->loadFile($pageData['template'])->render($pageData);
     }
 
     public function getTemplate()
@@ -309,84 +175,22 @@ class DefaultPage extends Object
 
         return $word;
     }
-}
 
-class Wrapper implements \ArrayAccess, \Iterator {
-	private $position = 0;
-	private $model;
-	private $data = null;
-
-	public function __construct($model) {
-		$this->model = $model;
-	}
-
-	public function offsetExists( $offset ) {
-		// TODO: Implement offsetExists() method.
-		return array_key_exists($offset, $this->fetch());
-	}
-
-	public function offsetGet( $offset ) {
-		$data =  $this->fetch();
-
-		return $data[$offset];
-	}
-
-	public function offsetSet( $offset, $value ) {
-		// TODO: Implement offsetSet() method.
-	}
-
-	public function offsetUnset( $offset ) {
-		// TODO: Implement offsetUnset() method.
-	}
-
-	public function setData($data) {
-		$this->data = $data;
-	}
-
-	public function next() {
-		++$this->position;
-	}
-
-	public function current() {
-		$item = $this->data[$this->position];
-		$wrapper = new Wrapper($this->model);
-		$wrapper->setData($item);
-
-		return $wrapper;
-	}
-
-	public function rewind() {
-		$this->position = 0;
-	}
-
-	public function key() {
-		return $this->position;
-	}
-
-	public function valid() {
-		$data = $this->fetch();
-
-		return isset($data[$this->position]);
-	}
-
-	private function fetch() {
-		if($this->data === null) {
-			$data = $this->model->fetch();
-
-			$name = $this->model->getName();
-			if(strpos($name, 'Channel') !== false) {
-				$name = strtolower(str_replace('Channel', '', $name));
+	private function _parseArray(&$array) {
+		foreach ( $array as $key => &$values ) {
+			if ( $key === 'template_config' ) {
+				foreach ( $values as $name => $value ) {
+					if ( $name === 'model' ) {
+						$array[ $name ] = new Wrappers\Model( $value, $this->container );
+					} else {
+						$array[ $name ] = new Wrappers\Config( $value );
+					}
+				}
 			}
 
-			if(array_key_exists('id', $this->model->getState()->getValues(true)) && strpos($this->model->getName(), 'Channel') === false) {
-				$name = Inflector::singularize($name);
+			if ( is_array( $values ) ) {
+				$this->_parseArray( $values);
 			}
-
-			error_log($name);
-
-			$this->data = $data[$name];
 		}
-
-		return $this->data;
 	}
 }
