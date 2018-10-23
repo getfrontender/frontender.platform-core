ef125883cb9f31a06a99b05f61379bb5a168395d
Jasper van Rijbroek <jasper@jvar.nl>
2018-10-23 16:27:24 +0200

    Removed user, pass and port.

--- lib/DB/Adapter.php
+++ lib/DB/Adapter.php
@@ -5,58 +5,64 @@ namespace Frontender\Core\DB;
 use function MongoDB\BSON\fromPHP;
 use function MongoDB\BSON\toJSON;
 
-class Adapter extends \MongoDB\Client {
-	static private $instance = null;
+class Adapter extends \MongoDB\Client
+{
+    static private $instance = null;
 
-	public function __construct() {
-		parent::__construct( 'mongodb://' . $_ENV['MONGO_HOST'] . ':' . $_ENV['MONGO_PORT'] . '/' );
-	}
+    public function __construct()
+    {
+        parent::__construct($_ENV['MONGO_HOST']);
+    }
 
-	public function collection( $collection ) {
-		return $this->selectCollection( $_ENV['MONGO_DB'], $collection );
-	}
+    public function collection($collection)
+    {
+        return $this->selectCollection($_ENV['MONGO_DB'], $collection);
+    }
 
-	public function toJSON( $docs, $assoc = false ) {
-		if(!$docs) {
-			return $docs;
-		}
+    public function toJSON($docs, $assoc = false)
+    {
+        if (!$docs) {
+            return $docs;
+        }
 
-		if ( is_array( $docs ) ) {
-			return array_map( function ( $document ) use ( $assoc ) {
-				$document = json_decode( toJSON( fromPHP( $document ) ), $assoc );
-				$this->replaceObjectID($document, $assoc);
+        if (is_array($docs)) {
+            return array_map(function ($document) use ($assoc) {
+                $document = json_decode(toJSON(fromPHP($document)), $assoc);
+                $this->replaceObjectID($document, $assoc);
 
-				return $document;
-			}, $docs );
-		}
+                return $document;
+            }, $docs);
+        }
 
-		$document = json_decode( toJSON( fromPHP( $docs ) ), $assoc );
-		$this->replaceObjectID($document, $assoc);
-		return $document;
-	}
+        $document = json_decode(toJSON(fromPHP($docs)), $assoc);
+        $this->replaceObjectID($document, $assoc);
+        return $document;
+    }
 
-	private function replaceObjectID(&$document, $isArray) {
+    private function replaceObjectID(&$document, $isArray)
+    {
 		// Check if we have an object or an array.
-		foreach($document as $key => &$value) {
-			if(!$isArray && is_object($value) && property_exists($value, '$oid')) {
-				$document->{$key} = $value->{'$oid'};
-			} else if(!$isArray && is_object($value)) {
-				$this->replaceObjectID($value, $isArray);
-			} else if($isArray && is_array($value) && array_key_exists('$oid', $value)) {
-				$document[$key] = $value['$oid'];
-			} else if($isArray && is_array($value)) {
-				$document[$key] = $this->replaceObjectID($value, $isArray);
-			}
-		}
+        foreach ($document as $key => &$value) {
+            if (!$isArray && is_object($value) && property_exists($value, '$oid')) {
+                $document->{$key} = $value->{'$oid'};
+            } else if (!$isArray && is_object($value)) {
+                $this->replaceObjectID($value, $isArray);
+            } else if ($isArray && is_array($value) && array_key_exists('$oid', $value)) {
+                $document[$key] = $value['$oid'];
+            } else if ($isArray && is_array($value)) {
+                $document[$key] = $this->replaceObjectID($value, $isArray);
+            }
+        }
 
-		return $document;
-	}
+        return $document;
+    }
 
-	static public function getInstance() {
-		if ( self::$instance === null ) {
-			self::$instance = new Adapter();
-		}
+    static public function getInstance()
+    {
+        if (self::$instance === null) {
+            self::$instance = new Adapter();
+        }
 
-		return self::$instance;
-	}
+        return self::$instance;
+    }
 }
