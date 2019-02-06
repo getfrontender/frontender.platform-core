8c24fd86e9ec1d634bbeff44798ca2a6b7ca4fa5
Babs Gösgens <babsgosgens@gmail.com>
2019-02-06 15:45:05 +0100

    Update App.php

--- lib/App.php
+++ lib/App.php
@@ -1,4 +1,17 @@
 <?php
+/*******************************************************
+ * Copyright (C) 2017-2019 Dipity B.V., The Netherlands
+ * 
+ * Frontender is a web application development platform consisting of a 
+ * desktop application (Frontender Desktop) and a web application which 
+ * consists of a client component (Frontender Platform) and a core 
+ * component (Frontender Platform Core). 
+ * This file is part of Frontender Platform Core. 
+ * 
+ * Frontender Desktop, Frontender Platform and Frontender Platform Core
+ * may not be copied and/or distributed without the express
+ * permission of Dipity B.V.
+ *******************************************************/
 
 namespace Frontender\Core;
 
