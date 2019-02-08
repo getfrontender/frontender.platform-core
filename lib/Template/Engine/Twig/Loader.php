a5c98ba450ecdd1696617872e3fd20dd9cadeed4
Leijten <jeroen@teemr.com>
2019-02-08 11:15:11 +0100

    Updated docblocks

--- lib/Template/Engine/Twig/Loader.php
+++ lib/Template/Engine/Twig/Loader.php
@@ -1,4 +1,18 @@
 <?php
+/*******************************************************
+ * @copyright 2017-2019 Dipity B.V., The Netherlands
+ * @package Frontender
+ * @subpackage Frontender Platform Core
+ *
+ * Frontender is a web application development platform consisting of a
+ * desktop application (Frontender Desktop) and a web application which
+ * consists of a client component (Frontender Platform) and a core
+ * component (Frontender Platform Core).
+ *
+ * Frontender Desktop, Frontender Platform and Frontender Platform Core
+ * may not be copied and/or distributed without the express
+ * permission of Dipity B.V.
+ *******************************************************/
 
 namespace Frontender\Core\Template\Engine\Twig;
 
--- lib/Template/Helper/Url.php
+++ lib/Template/Helper/Url.php
@@ -1,10 +1,18 @@
 <?php
-
-/**
- * @package     Dipity
- * @copyright   Copyright (C) 2014 - 2017 Dipity B.V. All rights reserved.
- * @link        http://www.dipity.eu
- */
+/*******************************************************
+ * @copyright 2017-2019 Dipity B.V., The Netherlands
+ * @package Frontender
+ * @subpackage Frontender Platform Core
+ *
+ * Frontender is a web application development platform consisting of a
+ * desktop application (Frontender Desktop) and a web application which
+ * consists of a client component (Frontender Platform) and a core
+ * component (Frontender Platform Core).
+ *
+ * Frontender Desktop, Frontender Platform and Frontender Platform Core
+ * may not be copied and/or distributed without the express
+ * permission of Dipity B.V.
+ *******************************************************/
 
 namespace Frontender\Core\Template\Helper;
 
