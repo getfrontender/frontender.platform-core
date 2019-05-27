ad7127531e9a15725a612029c4eb145d583394c4
Jasper van Rijbroek <jasper@jvar.nl>
2019-05-27 11:25:11 +0200

    Added roles endpoint.

---
+++ lib/Routes/Api/Roles.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Frontender\Core\Routes\Api;
+
+use Frontender\Core\Routes\Helpers\CoreRoute;
+use Slim\Http\Request;
+use Slim\Http\Response;
+use Frontender\Core\DB\Adapter;
+use Frontender\Core\Routes\Middleware\TokenCheck;
+use Frontender\Core\Routes\Middleware\ApiLocale;
+
+class Roles extends CoreRoute
+{
+    protected $group = '/api/roles';
+
+    public function registerReadRoutes()
+    {
+        parent::registerReadRoutes();
+
+        $this->app->get('/users/{user_id}', function (Request $request, Response $response) {
+            // The user is logged in.
+            $userRoles = Adapter::getInstance()->collection('roles')->find([
+                'users' => (int)$request->getAttribute('user_id')
+            ])->toArray();
+            $userRoles = Adapter::getInstance()->toJSON($userRoles);
+
+            return $response->withJson($userRoles);
+        });
+    }
+
+    public function getGroupMiddleware()
+    {
+        return [
+            new TokenCheck(
+                $this->app->getContainer()
+            ),
+            new ApiLocale($this->app->getContainer())
+        ];
+    }
+}
