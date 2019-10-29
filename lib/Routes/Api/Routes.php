d6f078236601ccafd3fd98106cbde415ac075db8
Jasper van Rijbroek <jasper@jvar.nl>
2019-10-29 12:02:48 +0100

    Added add update and remove endpoints.

--- lib/Routes/Api/Routes.php
+++ lib/Routes/Api/Routes.php
@@ -22,6 +22,8 @@ use Slim\Http\Request;
 use Slim\Http\Response;
 use Frontender\Core\App;
 use Frontender\Core\DB\Adapter;
+use MongoDB\BSON\ObjectId;
+
 
 class Routes extends CoreRoute {
     protected $group = '/api/routes';
@@ -32,6 +34,22 @@ class Routes extends CoreRoute {
         parent::__construct($container);
 
         $this->collection = Adapter::getInstance()->collection('routes');
+    }
+
+    public function registerCreateRoutes() {
+        $self = $this;
+
+        $this->app->put('', function(Request $request, Response $response) use ($self) {
+            // Insert the route into the database.
+            $self->collection->insertOne([
+                'resource' => $request->getParsedBodyParam('resource'),
+                'destination' => $request->getParsedBodyParam('destination'),
+                'type' => $request->getParsedBodyParam('type'),
+                'status' => (int) $request->getParsedBodyParam('status')
+            ]);
+
+            return $response->withStatus(200);
+        });
     }
 
     public function registerReadRoutes() {
@@ -65,6 +83,41 @@ class Routes extends CoreRoute {
             return $response->withJson(
                 Adapter::getInstance()->toJSON($landingpages)
             );
+        });
+    }
+
+    public function registerUpdateRoutes() {
+        parent::registerUpdateRoutes();
+
+        $self = $this;
+
+        $this->app->post('/{routeID}', function(Request $request, Response $response) use ($self) {
+            $self->collection->findOneAndUpdate([
+                '_id' => new ObjectID($request->getAttribute('routeID'))
+            ], [
+                '$set' => [
+                    'resource' => $request->getParsedBodyParam('resource'),
+                    'destination' => $request->getParsedBodyParam('destination'),
+                    'type' => $request->getParsedBodyParam('type'),
+                    'status' => (int) $request->getParsedBodyParam('status')
+                ]
+            ]);
+
+            return $response->withStatus(200);
+        });
+    }
+
+    public function registerDeleteRoutes() {
+        parent::registerDeleteRoutes();
+
+        $self = $this;
+
+        $this->app->delete('/{routeID}', function(Request $request, Response $response) use ($self) {
+            $self->collection->findOneAndDelete([
+                '_id' => new ObjectID($request->getAttribute('routeID'))
+            ]);
+
+            return $response->withStatus(200);
         });
     }
 }
