a22fbb90d71889b5d43fbee02231c54e171b4a65
Jasper van Rijbroek <jasper@jvar.nl>
2019-12-31 12:56:05 +0100

    Updated code styling.

--- lib/Installer/Project.php
+++ lib/Installer/Project.php
@@ -19,152 +19,128 @@ namespace Frontender\Core\Installer;
 use Composer\Script\Event;
 use Symfony\Component\Finder\Finder;
 
-class Project extends Base
-{
-    public static function import(Event $event)
-    {
-        $args = $event->getArguments();
+class Project extends Base {
+	public static function import( Event $event ) {
+		$args = $event->getArguments();
+		defined( 'ROOT_PATH' ) or define( 'ROOT_PATH', getcwd() );
 
-        // Requiring autoloader and .env file.
-        require_once getcwd() . '/vendor/autoload.php';
-        $env = new \Dotenv\Dotenv(getcwd());
-        $env->load();
+		// Requiring autoloader and .env file.
+		require_once getcwd() . '/vendor/autoload.php';
+		$env = new \Dotenv\Dotenv( getcwd() );
+		$env->load();
 
-        if (!count($args)) {
-            self::writeLn('No source specified!', 'red');
-            exit;
-        }
+		if ( ! count( $args ) ) {
+			self::writeLn( 'No source specified!', 'red' );
+			exit;
+		}
 
-        $source = $args[0];
-        $tempFile = self::getTempPath('project_file');
-        $tempDir = self::getTempPath('project_install', true);
+		$source   = $args[0];
+		$tempFile = self::getTempPath( 'project_file' );
+		$tempDir  = self::getTempPath( 'project_install', true );
 
-        self::writeLn('Downloading project', 'blue');
-        file_put_contents($tempFile, file_get_contents($source));
-        $zip = new \ZipArchive();
-        if ($zip->open($tempFile)) {
-            $zip->extractTo($tempDir);
-            $zip->close();
-        } else {
-            self::writeLn('Something went wrong when installing the project, please contact the project developer', 'red');
-            exit;
-        }
+		self::writeLn( 'Downloading project', 'blue' );
+		file_put_contents( $tempFile, file_get_contents( $source ) );
+		$zip = new \ZipArchive();
+		if ( $zip->open( $tempFile ) ) {
+			$zip->extractTo( $tempDir );
+			$zip->close();
+		} else {
+			self::writeLn( 'Something went wrong when installing the project, please contact the project developer', 'red' );
+			exit;
+		}
 
-        self::writeLn('Project downloaded, installing', 'blue');
-        self::importMongoFiles($tempDir);
-        self::importPublicFiles($tempDir);
-        self::importProjectFiles($tempDir);
-        self::importLibFiles($tempDir);
+		self::writeLn( 'Project downloaded, installing', 'blue' );
+		self::importPublicFiles( $tempDir );
+		self::importProjectFiles( $tempDir );
+		self::importLibFiles( $tempDir );
+		self::updateComposerJson( $tempDir );
+		self::importMongoFiles( getcwd() . '/project' );
 
-        self::writeLn('Project has been imported!', 'green');
-    }
+		self::writeLn( 'Project has been imported!', 'green' );
+	}
 
-    private static function importPublicFiles(string $dir): bool
-    {
-        $publicDir = self::findPathByDirName('public', $dir);
-        $finder = new Finder();
+	private static function importPublicFiles( string $dir ): bool {
+		$publicDir = self::findPathByDirName( 'public', $dir );
+		$finder    = new Finder();
 
-        if (!$publicDir) {
-            return false;
-        }
+		if ( ! $publicDir ) {
+			return false;
+		}
 
-        // Get all the files of the public directory, and move them.
-        $files = $finder->files()->in($publicDir);
-        foreach ($files as $file) {
-            $newFilePath = getcwd() . '/public/' . $file->getRelativePath();
+		// Get all the files of the public directory, and move them.
+		$files = $finder->files()->in( $publicDir );
+		foreach ( $files as $file ) {
+			$newFilePath = getcwd() . '/public/' . $file->getRelativePath();
 
-            @mkdir($newFilePath, 0777, true);
-            @rename($file->getRealPath(), $newFilePath . '/' . $file->getFileName());
-        }
+			@mkdir( $newFilePath, 0777, true );
+			@rename( $file->getRealPath(), $newFilePath . '/' . $file->getFileName() );
+		}
 
-        return true;
-    }
+		return true;
+	}
 
-    private static function importProjectFiles(string $dir): bool
-    {
-        $projectDir = self::findPathByDirName('project', $dir);
-        $finder = new Finder();
+	private static function importProjectFiles( string $dir ): bool {
+		$projectDir = self::findPathByDirName( 'project', $dir );
+		$finder     = new Finder();
 
-        if (!$projectDir) {
-            return false;
-        }
+		if ( ! $projectDir ) {
+			return false;
+		}
 
-        $directories = $finder->directories()->in($projectDir)->notName('db')->depth(0);
-        foreach ($directories as $directory) {
-            if($directory->getFileName() === 'adapters') {
-                self::importAdapters($directory->getRealPath());
-            } else {
-                $filesFinder = new Finder();
-                $files = $filesFinder->files()->in($directory->getRealPath());
+		$directories = $finder->directories()->in( $projectDir )->depth( 0 );
+		foreach ( $directories as $directory ) {
+			$filesFinder = new Finder();
+			$files       = $filesFinder->files()->in( $directory->getRealPath() );
 
-                foreach ($files as $file) {
-                    $newFilePath = getcwd() . '/project/' . $directory->getFileName() . '/' .  $file->getRelativePath();
+			foreach ( $files as $file ) {
+				$newFilePath = getcwd() . '/project/' . $directory->getFileName() . '/' . $file->getRelativePath();
 
-                    @mkdir($newFilePath, 0777, true);
-                    @rename($file->getRealPath(), $newFilePath . '/' . $file->getFileName());
-                }
-            }
-        }
+				@mkdir( $newFilePath, 0777, true );
+				@rename( $file->getRealPath(), $newFilePath . '/' . $file->getFileName() );
+			}
+		}
 
-        return true;
-    }
+		return true;
+	}
 
-    public static function importLibFiles(string $dir): bool
-    {
-        $libDir = self::findPathByDirName('lib', $dir);
-        $finder = new Finder();
+	public static function importLibFiles( string $dir ): bool {
+		$libDir = self::findPathByDirName( 'lib', $dir );
+		$finder = new Finder();
 
-        if (!$libDir) {
-            return false;
-        }
+		if ( ! $libDir ) {
+			return false;
+		}
 
-        $directories = $finder->directories()->in($libDir)->notName('db')->depth(0);
-        foreach ($directories as $directory) {
-            $filesFinder = new Finder();
-            $files = $filesFinder->files()->in($directory->getRealPath());
+		$directories = $finder->directories()->in( $libDir )->notName( 'db' )->depth( 0 );
+		foreach ( $directories as $directory ) {
+			$filesFinder = new Finder();
+			$files       = $filesFinder->files()->in( $directory->getRealPath() );
 
-            foreach ($files as $file) {
-                $newFilePath = getcwd() . '/lib/' . $directory->getFileName() . '/' .  $file->getRelativePath();
+			foreach ( $files as $file ) {
+				$newFilePath = getcwd() . '/lib/' . $directory->getFileName() . '/' . $file->getRelativePath();
 
-                @mkdir($newFilePath, 0777, true);
-                @rename($file->getRealPath(), $newFilePath . '/' . $file->getFileName());
-            }
-        }
+				@mkdir( $newFilePath, 0777, true );
+				@rename( $file->getRealPath(), $newFilePath . '/' . $file->getFileName() );
+			}
+		}
 
-        return true;
-    }
+		return true;
+	}
 
-    public static function importAdapters(string $directory):bool {
-        $filesFinder = new Finder();
-        $files = $filesFinder->files()->in($directory);
+	private static function updateComposerJson( string $directory ): void {
+		$filesFinder   = new Finder();
+		$composerFiles = $filesFinder->files()->in( $directory )->name( 'composer\.json' );
+		$composerJSON  = json_decode( file_get_contents( getcwd() . '/composer.json' ), true );
 
-        // Find all the composer.json files and merge them into one.
-        self::updateComposerJson($directory);
+		foreach ( $composerFiles as $file ) {
+			// Merge all the elements of the contents.
+			$contents = json_decode( $file->getContents(), true );
 
-        foreach ($files as $file) {
-            $newFilePath = getcwd() . '/lib/Model/' . $file->getRelativePath();
+			foreach ( $contents as $key => $value ) {
+				$composerJSON[ $key ] = array_merge( $composerJSON[ $key ], $value );
+			}
+		}
 
-            @mkdir($newFilePath, 0777, true);
-            @rename($file->getRealPath(), $newFilePath . '/' . $file->getFileName());
-        }
-
-        return true;
-    }
-
-    private static function updateComposerJson(string $directory): void {
-        $filesFinder = new Finder();
-        $composerFiles = $filesFinder->files()->in($directory)->name('composer\.json');
-        $composerJSON = json_decode(file_get_contents(getcwd() . '/composer.json'), true);
-
-        foreach($composerFiles as $file) {
-            // Merge all the elements of the contents.
-            $contents = json_decode($file->getContents(), true);
-
-            foreach($contents as $key => $value) {
-                $composerJSON[$key] = array_merge($composerJSON[$key], $value);
-            }
-        }
-
-        file_put_contents(getcwd() . '/composer.json', json_encode($composerJSON));
-    }
+		file_put_contents( getcwd() . '/composer.json', json_encode( $composerJSON ) );
+	}
 }
